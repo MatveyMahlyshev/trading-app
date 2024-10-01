ed753d93cd465fe22bdd05212d9aa39b58c54d36
MatveyMahlyshev <matthewfootballer11@gmail.com>
2024-10-01 21:33:37 +0300

    first migration

---
+++ trading/models/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy import JSON, Column, MetaData, Integer, String, TIMESTAMP, Table, ForeignKey
+
+metadata = MetaData()
+
+role = Table(
+    'role',
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("permissions", JSON),
+)
+
+user = Table(
+    'user',
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    Column("role_id", Integer, ForeignKey("role.id")),
+)
